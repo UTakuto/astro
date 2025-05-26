a6dcf080c8b4aba69fedca53e865ebb557694aba
UTakuto <taku.0307.ue@icloud.com>
2025-05-26 10:45:17 +0900

    title , description , x_name , urlの定数化

--- src/const.js
+++ src/const.js
@@ -1,1 +1,10 @@
+// 言語 ja を定数化
 export const lang = "ja";
+// title
+export const title = "Astro-Practice";
+// description
+export const description = "astro練習用でHTML3からサンプルや課題を参考にしています";
+// x_name
+export const x_name = "ecccomp";
+// url
+export const url = "https://comp.ecc.ac.jp/";
